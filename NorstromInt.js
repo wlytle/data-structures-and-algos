8fffe72fdad3dbc459e2b05ae3e5228e7fde9e88
wlytle <wlytle89@gmail.com>
2021-02-19 14:04:22 -0700

    matches values doesn't yet sort

--- NorstromInt.js
+++ NorstromInt.js
@@ -1,23 +1,27 @@
 function findMatching(s1, s2) {
+  first = s1.split(" ");
+  second = s2.split(" ");
+  console.log(first);
   let num = "",
     char = "",
     curr;
-  //iterate through each str skipping spaces
-  for (let i = 0; i < s1.length; i += 2) {
-    for (let j = 0; j < s1.length; j += 2) {
-      curr = s1[i];
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      curr = first[i];
       //if the character is in both strings and isn't a numeric add it to char
-      if (curr === s2[j] && Number.isNaN(+curr)) {
-        char = sort(char, curr);
+      if (curr === second[j] && Number.isNaN(+curr)) {
+        char += " " + curr;
+        // char = sort(char, curr);
         // if the charecter is in bot strs and is a numeric add it to num
-      } else if (cur === s2[j]) {
-        num = sort(num, curr);
+      } else if (curr === second[j]) {
+        num += " " + curr;
+        // num = sort(num, curr);
       }
     }
   }
-  return num + " " + char;
+  return num + char;
 }
 s1 = "1 2 3 A B C";
 s2 = "X 11 G M 2 9 3 C N R";
 
-console.log(findMatching(s1, s1));
+console.log(findMatching(s1, s2));
